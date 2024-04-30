dd8cb128a9628f4d57d1d4d928e7622bb6c8c3b9
Srinu1224 <168546782+Srinu1224@users.noreply.github.com>
2024-04-30 12:54:35 +0000

    Learnt Data Types

---
+++ DataTypes.js
@@ -0,0 +1,59 @@
+// Single Line Comment
+
+/*
+Multi-Line
+Comment
+*/
+
+//Data Types in JavaScript
+
+//Number => 123, 123.56, limited to 2 to the power of 53
+//String => " ", ' '
+//BigInt => 65652n
+//Boolean => true or false
+//Object => {}
+//Null => null
+//Undefined => undefined - when value is not assigned
+//Date => 01/01/1999
+//Array => []
+//Symbol => 
+
+const MyNumber=12345
+console.log(MyNumber);
+console.log(typeof MyNumber);
+console.log("____________________");
+
+
+const MyString = "This is Double Quoted String"
+console.log(MyString);
+console.log(typeof MyString);
+console.log("____________________");
+
+
+const MyString2 = 'This is Single Quoted String'
+console.log(MyString2);
+console.log(typeof MyString2);
+console.log('______"______"________');
+
+
+const myBigInt = 12677n
+console.log(myBigInt);
+console.log(typeof myBigInt);
+console.log('______"______"________');
+
+const myBool = 1656532
+console.log(myBool);
+console.log(typeof myBool);
+console.log('______"______"________');
+
+const MyNull = null
+console.log(MyNull);
+console.log(typeof MyNull);
+
+let my
+console.log(my);
+console.log(typeof my);
+
+const myArr = [1,2,3,4,5,6,7,8,9,0]
+console.log(myArr);
+console.log(typeof myArr);
