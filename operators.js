f730affcbdaa9f90ff2c73fb06639ac5e24576d2
Srinu1224 <168546782+Srinu1224@users.noreply.github.com>
2024-05-01 12:50:09 +0000

    learnt about operator

---
+++ operators.js
@@ -0,0 +1,44 @@
+/* Arithmetic Operators
++ => Addition, x + y
+- => Subtraction, x - y
+/ => Division, x / y
+* => Multiplication, x * y
+% => Modulus, x % y, x=10, y=2, value returned is 0
+** => 2 to the power of 4, 2**4, value will be 16
+*/
+
+/* Asignment Operator
+= => sum = x + y
++= => x = x + y, x += y
+-= => x -= y
+*= => x *= y
+/= => x /= y
+%=
+**=
+*/
+
+/* Comparison Operators
+< => x = 10, y = 2, x > y, result true.
+> => x = 10, y = 2, x < y, result false.
+== => compares values of two variables.
+=== => compares values and data type both.
+<= 
+>=
+!= 
+!==
+----------------------------------*/
+/* Unary Operator => takes only single operands
+++ => x++ (post increment), ++x (pre increment)
+-- => Decreament Operator
+! => NOT Operator
+*/
+
+
+
+let x = "12"
+let y = 12
+
+if (!(x!=y)) console.log("x is equal to y");
+
+// console.log(y++);
+// console.log(y);
